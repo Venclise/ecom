34b4eece60463668c4f315a5dc270d73358a5cdc
Venclise <zunnurainzahoor955@gmail.com>
2026-01-01 22:10:30 +0500

    Fixxy fix

--- app/api/orders/[id]/route.ts
+++ app/api/orders/[id]/route.ts
@@ -1,24 +1,31 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { connectDB } from "../../lib/db";
 import { Order } from "../../models/order";
 
 export async function GET(
-
-  {params}: {params: Promise<{ id: string }>}
+  request: NextRequest,
+  context: any 
 ) {
   try {
     await connectDB();
 
-    const { id } = await params;
+    const { id } = await context.params;
+
     const order = await Order.findById(id);
 
     if (!order) {
-      return NextResponse.json({ message: "Order not found" }, { status: 404 });
+      return NextResponse.json(
+        { message: "Order not found" },
+        { status: 404 }
+      );
     }
 
-    return NextResponse.json(order, { status: 200 });
+    return NextResponse.json(order);
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ message: "Failed to fetch order" }, { status: 500 });
+    return NextResponse.json(
+      { message: "Failed to fetch order" },
+      { status: 500 }
+    );
   }
 }
